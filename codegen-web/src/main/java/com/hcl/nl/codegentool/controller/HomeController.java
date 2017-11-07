40beb580712e661032c1e8015f4170cf89b0301f
Masud Hasan <masudbook@gmail.com>
2017-11-07 15:06:24 +0100

    updated package name

--- codegen-web/src/main/java/com/hcl/nl/codegentool/controller/HomeController.java
+++ codegen-web/src/main/java/com/hcl/nl/codegentool/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.hcl.nl.codegen.controller;
+package com.hcl.nl.codegentool.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
